cc7ec3fb38ca1e1001fee262f6543e5bd2c87156
engineerscodes <practicalengineers05@gmail.com>
2020-11-05 23:55:16 +0530

    intial Commit

---
+++ JavaScript/Basic JavaScript/06.project(Basic JavaScriptQuiz)/01. Starter Files/js/main.js
@@ -0,0 +1,10 @@
+//console.log("connected")
+
+var name=prompt("Enter your name:");
+
+var gender=prompt("Enter your Generder");
+
+var res="Your Name is "+name+"And your Gender is"+gender;
+
+console.log(res);
+document.write(res);
