29985a45b436d8ea0eda23a4fecdf26d841d0420
engineerscodes <practicalengineers05@gmail.com>
2020-11-05 22:17:11 +0530

    intial Commit

---
+++ JavaScript/Basic JavaScript/03.Syntax/main.js
@@ -0,0 +1,30 @@
+console.log("Connected")
+
+/*
+Variables In javaScript
+*/
+
+var x,y,z,str;  //Declaring variables
+x=100;
+y=50;        //assignment of values
+z=x+y;
+
+str="Naveen"
+
+
+//JavaScript Litterals
+10.001
+10
+"Strings"
+
+'String'
+
+//Java Scrpit Variables
+
+var x=9 ,y;  //the varible x is override here  but still y=50 because we have not assigned it value 
+console.log(x=7); //prints seven
+
+//Operators 
+var sum=5+(30/8)*100 -10;
+
+var t="naveen"+"rcb"; //naveenrcb
