ae001d5e629066352d1a99a2a364115d04a7e517
engineerscodes <practicalengineers05@gmail.com>
2020-11-05 22:42:25 +0530

    intial Commit

---
+++ JavaScript/Basic JavaScript/04.Statements/main.js
@@ -0,0 +1,11 @@
+//document.getElementsByClassName("test").innerHTML ="Hello World"; doesnot work
+
+document.getElementById('test').innerHTML ="Hello World";
+
+var x,y,z;
+
+x=0;
+y=1;
+z=2;
+
+x=3;y=5;z=7;
