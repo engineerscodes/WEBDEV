266ef3f601cdb60e36f206621dbdc2e0703875ef
engineerscodes <practicalengineers05@gmail.com>
2020-11-06 00:00:37 +0530

    Minor changes

--- JavaScript/Basic JavaScript/06.project(Basic JavaScriptQuiz)/01. Starter Files/js/main.js
+++ JavaScript/Basic JavaScript/06.project(Basic JavaScriptQuiz)/01. Starter Files/js/main.js
@@ -4,7 +4,7 @@ var name=prompt("Enter your name:");
 
 var gender=prompt("Enter your Generder");
 
-var res="Your Name is "+name+"And your Gender is"+gender;
+var res="Your Name is "+name+" And your Gender is"+gender;
 
 console.log(res);
 document.write("<p>"+res+"</p>");
