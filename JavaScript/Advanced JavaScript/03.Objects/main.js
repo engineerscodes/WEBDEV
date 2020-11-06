7e407dcd4a81c20f90bf7eaac3c409a4484ea80b
engineerscodes <practicalengineers05@gmail.com>
2020-11-06 17:58:02 +0530

    Initial Commit

--- JavaScript/Advanced JavaScript/03.Objects/main.js
+++ JavaScript/Advanced JavaScript/03.Objects/main.js
@@ -1,7 +1,11 @@
 var user =
 {name:"NAVEEN",
 gender :"Male",
-Age:19
+Age:19,
+fulluser :function ()
+{
+	return this.name+" is "+this.gender +" and "+this.Age+" yr Old."; //must use this keyword
+}
 };
 
-document.getElementById('test').innerHTML= user.name+" "+user.Age;
+document.getElementById('test').innerHTML= user.fulluser();
